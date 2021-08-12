d0a885c10d9a1e47aebd011e030d50aacda31d03
dw3nt <rick@dualwieldent.com>
2021-08-12 14:27:28 -0700

    door instances don't share collider shapes

--- objects/MetalDoor.gd
+++ objects/MetalDoor.gd
@@ -1,6 +1,12 @@
 extends StaticBody2D
 
 onready var animation = $AnimationPlayer
+onready var collider = $CollisionShape2D
+
+
+func _ready():
+	var newColliderShape = collider.shape.duplicate()
+	collider.shape = newColliderShape
 
 
 func openDoor():
--- objects/WoodDoor.gd
+++ objects/WoodDoor.gd
@@ -5,10 +5,18 @@ export(bool) var isOpen = false setget setIsOpen, getIsOpen
 
 onready var costLabel = $CostLabel
 onready var animation = $AnimationPlayer
+onready var topCollider = $TopCollider
+onready var bottomCollider = $BottomCollider
 
 
 func _ready():
 	costLabel.text = str(doorCost)
+	
+	var newTopColliderShape = topCollider.shape.duplicate()
+	topCollider.shape = newTopColliderShape
+	
+	var newBottomColliderShape = bottomCollider.shape.duplicate()
+	bottomCollider.shape = newBottomColliderShape
 	
 	
 func setIsOpen(val):
