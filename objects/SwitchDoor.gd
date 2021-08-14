84f62b762ab2bfa797cb195eb56a5181037dd1a9
dw3nt <rick@dualwieldent.com>
2021-08-14 11:11:49 -0700

    switch door open close sounds

--- objects/SwitchDoor.gd
+++ objects/SwitchDoor.gd
@@ -2,6 +2,8 @@ extends StaticBody2D
 
 onready var animation = $AnimationPlayer
 onready var collider = $CollisionShape2D
+onready var doorOpenAudio = $DoorOpenAudio
+onready var doorCloseAudio = $DoorCloseAudio
 
 
 func _ready():
@@ -11,7 +13,9 @@ func _ready():
 
 func openDoor():
 	animation.play("slide_up")
+	doorOpenAudio.play()
 	
 	
 func closeDoor():
 	animation.play_backwards("slide_up")
+	doorCloseAudio.play()
